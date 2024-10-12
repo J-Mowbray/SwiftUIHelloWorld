5fe39e9c8a2760d072a4aff59a0400b8ef9ed64c
J-Mowbray <j.mowbray96@gmail.com>
2024-10-12 15:49:06 +0100

    Initial Commit

---
+++ SwiftUIHelloWorld/SwiftUIHelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIHelloWorldApp.swift
+//  SwiftUIHelloWorld
+//
+//  Created by Jamie Mowbray on 12/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIHelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftUIHelloWorldTests/SwiftUIHelloWorldTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIHelloWorldTests.swift
+//  SwiftUIHelloWorldTests
+//
+//  Created by Jamie Mowbray on 12/10/2024.
+//
+
+import Testing
+@testable import SwiftUIHelloWorld
+
+struct SwiftUIHelloWorldTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
